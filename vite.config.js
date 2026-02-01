69936cec2ae219ea1849e40b0683ab719fb5d6d4
Ali-Ellithey <alisayed1519@gmail.com>
2026-02-01 16:41:38 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  // استبدل project-web-main باسم المستودع كما يظهر في رابط GitHub
-  base: '/project-web-main/',
+  // تأكد من كتابة اسم المستودع (Repo) الخاص بك بشكل صحيح بين السلاش
+  base: '/wajeh-Alealamia/',
 
   plugins: [react()],
 
   build: {
+    // هذا المجلد هو الذي سيقرأ منه GitHub Pages
     outDir: 'docs',
     chunkSizeWarningLimit: 1600,
   },
