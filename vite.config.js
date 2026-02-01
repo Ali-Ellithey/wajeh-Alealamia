64e0b9d5b4d8a0a55e28c9a108f31a3a53dda0a7
Ali-Ellithey <alisayed1519@gmail.com>
2026-02-01 16:55:07 +0200

    Update docs with correct base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // تأكد من كتابة اسم المستودع (Repo) الخاص بك بشكل صحيح بين السلاش
   base: '/wajeh-Alealamia/',
 
   plugins: [react()],
